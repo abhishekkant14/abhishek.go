eaf617959f08ef0e38124638555bb87add024fa4
Abhishek <Abhishekkant051@gmail.com>
2025-07-14 12:33:26 +0530

    switch condition

--- 19Switch/main.go
+++ 19Switch/main.go
@@ -1,14 +1,29 @@
 package main
-import "fmt"
 
+import "fmt"
 func main() {
 
-evenumber:=0
+evenCount:=0
 oddnumber:=0
-for i:=1; i<=10; i++{
-switch 1 % 2
-case 0:
-	fmt.Println(i,"even")
+for i:=1; i<=10; i++ {
+	switch i % 2 {
+	case 0:
+	
+		fmt.Println(i,"evenCount")
+		evenCount++
+
+	case 1:
+		fmt.Println(i,"oddnumber")
+		oddnumber++
+
+
+	}
+	fmt.Println("Number of 9 to 15 is:")
+	for i:=9; i<=15 ; i++{
+		fmt.Println(i,"Number")
+	}
 
 }
+fmt.Println("Total EvenNumber is",evenCount)
+fmt.Println("Total OddNumber is",oddnumber)
 }
